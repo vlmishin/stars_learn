10b74e6b9db6db7b8800848ee30a8f7982203528
vladimir <vl.mishin.99@gmail.com>
2026-02-20 16:50:42 +0300

    Added initial README structure

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-
 from .models import Course, Question, Answer, TestAttempt, UserAnswer, Lesson
 
 
